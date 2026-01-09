7e960711c9c6d62502c516528520ee60c8b07da8
Y2om <ybhss1418@naver.com>
2026-01-09 00:36:56 +0000

    Feat: .gitignore 작성

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from videos.main import router as video_router
 app = FastAPI()
 
 # 두 마을(Prefix)을 모두 연결해줘요
-app.include_router(user_router)
-app.include_router(video_router)
-
+app.include_router(user_router, prefix="/users")
+app.include_router(video_router, prefix="/videos")
 
 # Backend/main.py
 from users.database import init_db
